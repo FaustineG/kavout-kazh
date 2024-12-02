3ea87851dd2b05b530e338fd45d916ca95e38344
faustine godbillot <faustine@fofo-laptop-perso.home>
2024-12-02 21:22:41 +0100

    env var

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,7 +2,7 @@ import type { HistoryAction, MoveCatPayload } from '@/models/action'
 import type { Cat } from '@/models/cat'
 import type { CatState } from '@/models/state'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = import.meta.env.VITE_API_URL
 
 export const getHistoryActions = (): Promise<HistoryAction[]> =>
   fetch(`${API_URL}/actions`).then((res) => res.json())
